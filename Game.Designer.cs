f2b446006a499ffd837ef1df5a701e734a864bf6
tolunayesergun <tolunay50@gmail.com>
2020-11-14 18:17:28 +0300

    Game Interface changed

---
+++ Game.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace GoldHunterAIGame
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlBoard = new System.Windows.Forms.Panel();
+            this.screenControlButton = new System.Windows.Forms.Button();
+            this.playerA = new System.Windows.Forms.Button();
+            this.playerB = new System.Windows.Forms.Button();
+            this.playerC = new System.Windows.Forms.Button();
+            this.playerD = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.MoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.TurnTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pnlBoard
+            // 
+            this.pnlBoard.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlBoard.Location = new System.Drawing.Point(10, 6);
+            this.pnlBoard.Name = "pnlBoard";
+            this.pnlBoard.Size = new System.Drawing.Size(700, 700);
+            this.pnlBoard.TabIndex = 0;
+            this.pnlBoard.Resize += new System.EventHandler(this.pnlBoard_Resize);
+            // 
+            // screenControlButton
+            // 
+            this.screenControlButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.screenControlButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.screenControlButton.Location = new System.Drawing.Point(1096, 6);
+            this.screenControlButton.Name = "screenControlButton";
+            this.screenControlButton.Size = new System.Drawing.Size(127, 33);
+            this.screenControlButton.TabIndex = 1;
+            this.screenControlButton.Text = "Tam Ekran Yap";
+            this.screenControlButton.UseVisualStyleBackColor = true;
+            this.screenControlButton.Click += new System.EventHandler(this.screenControlButton_Click);
+            // 
+            // playerA
+            // 
+            this.playerA.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.playerA.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.playerA.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.playerA.Location = new System.Drawing.Point(758, 12);
+            this.playerA.Name = "playerA";
+            this.playerA.Size = new System.Drawing.Size(34, 36);
+            this.playerA.TabIndex = 2;
+            this.playerA.Text = "A";
+            this.playerA.UseVisualStyleBackColor = false;
+            // 
+            // playerB
+            // 
+            this.playerB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.playerB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.playerB.Location = new System.Drawing.Point(798, 12);
+            this.playerB.Name = "playerB";
+            this.playerB.Size = new System.Drawing.Size(34, 36);
+            this.playerB.TabIndex = 3;
+            this.playerB.Text = "B";
+            this.playerB.UseVisualStyleBackColor = false;
+            // 
+            // playerC
+            // 
+            this.playerC.BackColor = System.Drawing.Color.Crimson;
+            this.playerC.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.playerC.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.playerC.Location = new System.Drawing.Point(838, 12);
+            this.playerC.Name = "playerC";
+            this.playerC.Size = new System.Drawing.Size(34, 36);
+            this.playerC.TabIndex = 4;
+            this.playerC.Text = "C";
+            this.playerC.UseVisualStyleBackColor = false;
+            // 
+            // playerD
+            // 
+            this.playerD.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.playerD.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.playerD.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.playerD.Location = new System.Drawing.Point(878, 12);
+            this.playerD.Name = "playerD";
+            this.playerD.Size = new System.Drawing.Size(34, 36);
+            this.playerD.TabIndex = 5;
+            this.playerD.Text = "D";
+            this.playerD.UseVisualStyleBackColor = false;
+            // 
+            // resultBox
+            // 
+            this.resultBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.resultBox.Location = new System.Drawing.Point(729, 125);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(465, 26);
+            this.resultBox.TabIndex = 6;
+            // 
+            // MoveTimer
+            // 
+            this.MoveTimer.Interval = 200;
+            this.MoveTimer.Tick += new System.EventHandler(this.MoveA_Tick);
+            // 
+            // TurnTimer
+            // 
+            this.TurnTimer.Tick += new System.EventHandler(this.TurnTimer_Tick);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1235, 714);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.playerD);
+            this.Controls.Add(this.playerC);
+            this.Controls.Add(this.playerB);
+            this.Controls.Add(this.playerA);
+            this.Controls.Add(this.screenControlButton);
+            this.Controls.Add(this.pnlBoard);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Game";
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.Game_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBoard;
+        private System.Windows.Forms.Button screenControlButton;
+        private System.Windows.Forms.Button playerA;
+        private System.Windows.Forms.Button playerB;
+        private System.Windows.Forms.Button playerC;
+        private System.Windows.Forms.Button playerD;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Timer MoveTimer;
+        private System.Windows.Forms.Timer TurnTimer;
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,262 @@
+﻿using GoldHunterAIGame.Models;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GoldHunterAIGame
+{
+    public partial class Game : Form
+    {
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        public static int areaXSize = 10;   // Oyunda ki bir satırda ki kare sayısı
+        public static int areaYSize = 10;   // Oyunda ki bir stunda ki kare sayısı
+        public static int goldRate = 20;    // Oyunda ki karelin % kaçının altın olduğunu tutan statik değişken
+        public static int secretGoldRate = 10;  // Oyunda ki karelin % kaçının gizli altın olduğunu tutan statik değişken
+        public static int turnMoveMAX = 3;  // Bir oyuncunun max hamle sayısını tutan statik değişken
+
+        public static int MoveNextTargetTEMP = 0;  // Gidilicek sonra ki hedefi tutan temp değişken, Buton numarası olarak hedef alıyor.
+        public static Button playerButtonTEMP;   // Sırada ki oyuncuyu tutan temp değişken, "A","B","C","D" olarak değer alıyor
+        public static int turnMoveTEMP = 1;     // Sırası gelen oyuncunun hamle sayısını tutan temp değişken
+
+        public static string[] playerTurnList = { "A", "B", "C", "D" };  // Oyuncuların sırasını tutan statik Dizi
+        public static int[] targetList = { 25, 5, 6, 7 };  // Oyuncuların sonraki hedeflerini tutan statik dizi
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+            CreateBoard();
+
+            TurnTimer.Start();
+ 
+        }
+
+        #region GlobalFunctions
+
+        private int FindButtonNumber(Cordinant map)
+        {
+            int result = ((map.row - 1) * areaXSize + map.column);
+            return result;
+        }   // satır ve stun bilgisi verilen butonun Numarasını veriyor.
+
+        private Cordinant FindCordinant(int buttonNumber)
+        {
+            Cordinant map = new Cordinant
+            {
+                row = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(buttonNumber) / Convert.ToDouble(areaXSize)))
+            };
+            int rowLimit = map.row * areaXSize;
+            map.column = areaXSize - (rowLimit - buttonNumber);
+            return map;
+        }  // Numarası verilen butonun satır ve stun bilgisini veriyor.
+
+        #endregion GlobalFunctions
+
+        #region GameDynamics
+
+        public static int playerTurn = 0;  // Sıranın hangi oyuncuda olduğunu tutuyor.
+
+        private void TurnTimer_Tick(object sender, EventArgs e)   // Oyun sırasını hareketlendiren timer
+        {
+            if (MoveTimer.Enabled == false)
+            {
+                if (playerTurn == 4) playerTurn = 0;
+                turnMoveTEMP = 1;
+                NextMove(playerTurnList[playerTurn], targetList[playerTurn]);
+                playerTurn++;
+            }
+        }
+
+        private void NextMove(string player, int target)
+        {
+            switch (player)
+            {
+                case "A":
+                    playerButtonTEMP = playerA;
+                    MoveNextTargetTEMP = target;
+                    MoveTimer.Start();
+                    break;
+
+                case "B":
+                    playerButtonTEMP = playerB;
+                    MoveNextTargetTEMP = target;
+                    MoveTimer.Start();
+                    break;
+
+                case "C":
+                    playerButtonTEMP = playerC;
+                    MoveNextTargetTEMP = target;
+                    MoveTimer.Start();
+                    break;
+
+                case "D":
+                    playerButtonTEMP = playerD;
+                    MoveNextTargetTEMP = target;
+                    MoveTimer.Start();
+                    break;
+
+                default:
+                    break;
+            }
+        }  // Sonraki hamleyi timer'a yollayan fonksiyon.
+
+        private void moveTo(Button player, int target)
+        {
+            Cordinant startSpot = FindCordinant(Convert.ToInt32(player.AccessibleName)), endSpot = FindCordinant(target);
+
+            if (startSpot.row < endSpot.row)
+            {
+                startSpot.row++;
+                player.Location = (pnlBoard.Controls["btn" + (Convert.ToInt32(player.AccessibleName) + areaXSize)] as Button).Location;
+                player.AccessibleName = (Convert.ToInt32(player.AccessibleName) + areaXSize).ToString();
+            }
+            else if (startSpot.row > endSpot.row)
+            {
+                startSpot.row--;
+                player.Location = (pnlBoard.Controls["btn" + (Convert.ToInt32(player.AccessibleName) - areaXSize)] as Button).Location;
+                player.AccessibleName = (Convert.ToInt32(player.AccessibleName) - areaXSize).ToString();
+            }
+            else if (startSpot.column < endSpot.column)
+            {
+                startSpot.column++;
+                player.Location = (pnlBoard.Controls["btn" + (Convert.ToInt32(player.AccessibleName) + 1)] as Button).Location;
+                player.AccessibleName = (Convert.ToInt32(player.AccessibleName) + 1).ToString();
+            }
+            else if (startSpot.column > endSpot.column)
+            {
+                startSpot.column--;
+                player.Location = (pnlBoard.Controls["btn" + (Convert.ToInt32(player.AccessibleName) - 1)] as Button).Location;
+                player.AccessibleName = (Convert.ToInt32(player.AccessibleName) - 1).ToString();
+            }
+
+            if (Convert.ToInt32(player.AccessibleName) == target) MoveTimer.Stop();
+        } // Sonraki hamleyi gerçekleştiren fonksiyon, Oyuncu ve hedef seçimi yapılması yeterli
+
+        private void MoveA_Tick(object sender, EventArgs e)
+        {
+            if (turnMoveTEMP == turnMoveMAX) MoveTimer.Stop();
+            moveTo(playerButtonTEMP, MoveNextTargetTEMP);
+            turnMoveTEMP++;
+        }  // Player ı hareketlendiren timer
+
+        #endregion GameDynamics
+
+        #region InterfaceFunctions
+
+        private void CreateBoard()
+        {
+            pnlBoard.Controls.Clear();
+
+            int areaTotalSize = areaXSize * areaYSize;   // Oyun alanında ki toplam kare sayısı
+            int cellWidth = Convert.ToInt32(Math.Floor(Convert.ToDouble(pnlBoard.Width) / areaXSize));  // Alandaki Bir karenin genişliği
+            int cellHeight = Convert.ToInt32(Math.Floor(Convert.ToDouble(pnlBoard.Height) / areaYSize)); // Alandaki Bir karenin uzunluğu
+            int totalGoldCount = (areaTotalSize * goldRate) / 100; // Alandaki Toplam Altın Sayısı
+            int[] goldSpawns = new int[4]; // Alandaki altınların yeri (Buton numarası olarak)
+
+            int playerAFirstSpawn = 1;
+            int playerBFirstSpawn = areaXSize;
+            int playerCFirstSpawn = areaTotalSize - (areaXSize - 1);
+            int playerDFirstSpawn = areaTotalSize;
+
+            Random rnd = new Random();
+
+            //Altınlar için rasgele yer türetiyoruz.
+            for (int i = 0; i < 4; i++)
+            {
+                int nextNumber = rnd.Next(5);
+                bool NotContains = goldSpawns.Contains(nextNumber);
+                if (NotContains) i--;
+                else goldSpawns[i] = nextNumber;
+            }
+
+            resultBox.Text = String.Join("-", goldSpawns);
+            int ButtonCount = 0;
+            int x = 0, y = 0;
+            for (int i = 0; i < areaTotalSize; i++)
+            {
+                string goldText = "0";
+                for (int j = 0; j < goldSpawns.Length; j++)
+                {
+                    if (goldSpawns[j] == i) goldText = (j + 1).ToString();
+                }
+
+                Button btn = new Button
+                {
+                    Size = new System.Drawing.Size(cellWidth, cellHeight),
+                    BackColor = Color.White,
+                    ForeColor = Color.Gray,
+                    Text = (i + 1).ToString(),
+                    Name = "btn" + (i + 1),
+                    Enabled = false,
+                    AccessibleName = goldText,
+                    Font = new System.Drawing.Font("Microsoft Sans Serif", 6.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)))
+                };
+
+                if (ButtonCount < areaXSize)
+                {
+                    btn.Location = new Point(x, y);
+                    pnlBoard.Controls.Add(btn);
+                    x += cellWidth;
+                    ButtonCount++;
+                }
+                else
+                {
+                    ButtonCount = 1;
+                    x = 0;
+                    y += cellHeight;
+                    btn.Location = new Point(x, y);
+                    pnlBoard.Controls.Add(btn);
+                    x += cellWidth;
+                }
+            }
+
+            pnlBoard.Controls.Add(playerA);
+            playerA.BringToFront();
+            playerA.Size = new System.Drawing.Size(cellWidth, cellHeight);
+            playerA.Location = (pnlBoard.Controls["btn" + playerAFirstSpawn.ToString()] as Button).Location;
+            playerA.AccessibleName = playerAFirstSpawn.ToString();
+
+            pnlBoard.Controls.Add(playerB);
+            playerB.BringToFront();
+            playerB.Size = new System.Drawing.Size(cellWidth, cellHeight);
+            playerB.Location = (pnlBoard.Controls["btn" + playerBFirstSpawn.ToString()] as Button).Location;
+            playerB.AccessibleName = playerBFirstSpawn.ToString();
+
+            pnlBoard.Controls.Add(playerC);
+            playerC.BringToFront();
+            playerC.Size = new System.Drawing.Size(cellWidth, cellHeight);
+            playerC.Location = (pnlBoard.Controls["btn" + playerCFirstSpawn.ToString()] as Button).Location;
+            playerC.AccessibleName = playerCFirstSpawn.ToString();
+
+            pnlBoard.Controls.Add(playerD);
+            playerD.BringToFront();
+            playerD.Size = new System.Drawing.Size(cellWidth, cellHeight);
+            playerD.Location = (pnlBoard.Controls["btn" + playerDFirstSpawn.ToString()] as Button).Location;
+            playerD.AccessibleName = playerDFirstSpawn.ToString();
+        }    // Oyun sahasını oluşturan fonksiyon
+
+        private void pnlBoard_Resize(object sender, EventArgs e)
+        {
+            CreateBoard();
+        }   // Oyun ekranının değişmesi durumunda oyunun tekrardan oluşması için gerekli trigger
+
+        private void screenControlButton_Click(object sender, EventArgs e)
+        {
+            if (screenControlButton.Text == "Tam Ekran Yap")
+            {
+                WindowState = FormWindowState.Maximized;
+                screenControlButton.Text = "Ekranı Küçült";
+            }
+            else
+            {
+                WindowState = FormWindowState.Normal;
+                screenControlButton.Text = "Tam Ekran Yap";
+            }
+        }  // Tam ekran yapmak için gerekli olan trigger
+
+        #endregion InterfaceFunctions
+    }
+}
